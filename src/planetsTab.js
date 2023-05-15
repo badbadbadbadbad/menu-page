c0228bdb5a16fe3aee3bc43d2d564d2d5cdfda03
Oliver <120849619+badbadbadbadbad@users.noreply.github.com>
2023-05-15 15:19:43 +0200

    Add assets for planet tab

---
+++ src/planetsTab.js
@@ -0,0 +1,20 @@
+const createPlanets = () => {
+  const planetsSection = document.createElement("div");
+  planetsSection.setAttribute("id", "about");
+
+  return planetsSection;
+};
+
+export default createPlanets;
+
+// Grid: 6 rows in fullscreen, 3 rows in half?
+// Use gap between elements
+
+// Each element is in a rounded borderless box
+// Each element uses a slightly opaque black background
+
+// Each element has a small image or icon
+// Each element has the planet name
+// Each element has two bullet points with a description
+
+//
